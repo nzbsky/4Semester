f05e52441279c7d5b010a1155920b655721c651e
nzbsky <150057133+nzbsky@users.noreply.github.com>
2025-06-23 18:59:39 +0300

    р

--- hw3/script.js
+++ hw3/script.js
@@ -1,22 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const lazyImages = document.querySelectorAll("img.lazy");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const lazyImages = document.querySelectorAll("img.lazy");
     
-    const imageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const img = entry.target;
-                const src = img.getAttribute("data-src");
+//     const imageObserver = new IntersectionObserver((entries, observer) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 const img = entry.target;
+//                 const src = img.getAttribute("data-src");
                 
-                if (src) {
-                    img.setAttribute("src", src);
-                    img.classList.add("loaded");
-                    observer.unobserve(img);
-                }
-            }
-        });
-    });
+//                 if (src) {
+//                     img.setAttribute("src", src);
+//                     img.classList.add("loaded");
+//                     observer.unobserve(img);
+//                 }
+//             }
+//         });
+//     });
     
-    lazyImages.forEach(img => {
-        imageObserver.observe(img);
+//     lazyImages.forEach(img => {
+//         imageObserver.observe(img);
+//     });
+// });
+
+const imeges = document.querySelectorAll(".container img.lazy")
+const onEntry = (elements, observer) => {
+    elements.forEach(element => {
+        console.log(element);
+        if(element.isIntersecting){
+             const img = element.target;
+             const src = img.getAttribute("data-src");
+             if (src) {
+                    // img.setAttribute("src", src)
+                    // observer.unobserve(img);
+                    img.src = src
+                    observer.unobserve(img);
+                 }
+             }
+        
     });
-});
+}
+console.log(imeges);
+const observer = new IntersectionObserver(onEntry, {threshold: 0.5})
+
+imeges.forEach(img => {
+    observer.observe(img)
+})
